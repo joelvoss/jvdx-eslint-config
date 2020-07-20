46701276225fa8069037b5a340c99ef1f733415f
Joel Voss <mail@joelvoss.com>
2020-07-20 19:30:51 +0200

    feat(prettier): refactor prettier to use sharable config

--- index.js
+++ index.js
@@ -109,19 +109,7 @@ module.exports = {
 
   rules: {
     // Prettier
-    'prettier/prettier': ['error',
-      {
-        printWidth: 80,
-        tabWidth: 2,
-        useTabs: false,
-        semi: true,
-        singleQuote: true,
-        trailingComma: 'all',
-        bracketSpacing: true,
-        jsxBracketSameLine: false,
-        arrowParens: 'avoid',
-      }
-    ],
+    'prettier/prettier': ['error', require("@jvdx/prettier-config-jvdx")],
 
     // http://eslint.org/docs/rules/
     'array-callback-return': 'warn',
