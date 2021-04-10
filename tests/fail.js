78628ce56c433c5b9a6db9c067d9a49f4b66d059
Joel Voss <mail@joelvoss.com>
2021-04-10 17:38:08 +0200

    chore: update dependencies and add proper tests

---
+++ tests/fail.js
@@ -0,0 +1,5 @@
+function sayHello() {
+  return 'Hello'
+}
+
+console.log(sayHello() + 'World');
---
+++ tests/pass.js
@@ -0,0 +1,6 @@
+function sayHello() {
+	return 'Hello';
+}
+
+// eslint-disable-next-line no-console
+console.log(sayHello() + 'World');
